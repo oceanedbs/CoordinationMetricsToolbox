259aa0dcbe73b2f762749bc551527a91e35cc8d1
oceanedbs <oceane.tribudubois@gmail.com>
2024-11-10 22:57:07 +0100

    Update __init__.py

--- coordination_metrics_toolbox/__init__.py
+++ coordination_metrics_toolbox/__init__.py
@@ -2,4 +2,4 @@ from .metrics import CoordinationMetrics
 from .metrics import generate_palette   
 
 
-__all__ = ['CoordinationMetrics', 'generate_palette']
+__all__ = ['CoordinationMetrics', 'generate_palette', 'get_pca_frame']
